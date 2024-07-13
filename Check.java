b9f88130a63edf2a123269a6efbc21039fb3afb4
muskankousar147 <muskankousar@gmail.com>
2024-07-13 13:34:39 +0530

    program

---
+++ Check.java
@@ -0,0 +1,12 @@
+public class Check{
+public static void main(String args[]){
+int number = 24;
+if(number%2==0){
+System.out.println("number is even");
+}else{
+System.out.println("number is odd");
+}
+
+
+}
+}
