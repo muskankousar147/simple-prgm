281d164a4946bc107bf776054e3947944cb4528c
muskankousar147 <muskankousar@gmail.com>
2024-07-13 13:24:51 +0530

    program

---
+++ ConditionCheck.java
@@ -0,0 +1,12 @@
+public class  ConditionCheck{
+public static void main(String args[]){
+for(int index=1; index<=50; index++){
+	if(index%2==0){
+	System.out.println("number is even");}
+	else{
+		System.out.println("number is odd");
+	}
+System.out.println("numbers from 1 to 50:"+index);
+}
+}
+}
